a8455326e7b1be3a2e5a46d17974785c98a7bad6
LITHISHWARAN <lithishwaran.v.s75@kalvium.community>
2025-03-07 10:46:28 +0530

    Milestone 19 complected

--- frontend/src/pages/productDetails.jsx
+++ frontend/src/pages/productDetails.jsx
@@ -437,7 +437,7 @@ export default function ProductDetails() {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
 	const [quantity, setQuantity] = useState(1);
-	const email = "gowrishvarma@gmail.com"
+	const email = "lithish@gmail.com"
 
 	useEffect(() => {
 		const fetchProduct = async () => {
