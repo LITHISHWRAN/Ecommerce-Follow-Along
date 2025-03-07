87acf0ca6a65c3480e5bb5a8a775844503671ccb
LITHISHWARAN <lithishwaran.v.s75@kalvium.community>
2025-03-07 11:37:45 +0530

    Milestone 20 completed

--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -4,7 +4,8 @@ import Home from "../pages/Home";
 import CreateProduct from "../pages/createProduct";
 import MyProducts from "../pages/myproducts";
 import Cart from "../pages/cart";
-import ProductDetails from "../pages/productDetails";   
+import ProductDetails from "../pages/productDetails"; 
+import Profile from "../pages/profile";  
 export {
-    LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails
+    LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile
 }
