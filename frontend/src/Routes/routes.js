2f064587689c943192f9e85bb8f9ca8cffc84d5b
LITHISHWARAN <lithishwaran.v.s75@kalvium.community>
2025-03-14 10:30:36 +0530

    Milestone 24 Completed

--- frontend/src/Routes/routes.js
+++ frontend/src/Routes/routes.js
@@ -8,6 +8,7 @@ import ProductDetails from "../pages/productDetails";
 import Profile from "../pages/profile";  
 import CreateAddress from "../pages/createAddress";
 import SelectAddress from "../pages/SelectAddress";
+import OrderConfirmation from "../pages/OrderConfirmation";
 export {
-    LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress
+    LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress,OrderConfirmation
 }
